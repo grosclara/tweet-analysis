e79df8fa5f76ef5c5c444168114c9dd00dd8e6d1
Clara <clara.gros@student-cs.fr>
2020-08-08 16:38:09 +0200

    Connecting to the Twitter API

---
+++ twitter_collect/credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @grosclara
+
+# Consume:
+CONSUMER_KEY='SAFTaJgX0A9rvHjxzbI0aNcxc'
+CONSUMER_SECRET='A3oWj5E85OItiXglGpeqtGAYpweHoPEeCGQobBnvJnDVqYhZDP'
+
+# Access:
+ACCESS_TOKEN='1062264785071419393-lzVX0JOMJDtdodI4uaDXbAPUh2ivqt'
+ACCESS_SECRET='tw9ZIsj6LxGRn4KkP1TXop8mfZ3BzzDiQGWWncKWLgHTg'
