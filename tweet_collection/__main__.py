c36da5c0d9b32cd583596bf1e29136c88d5e7314
Clara <clara.gros@student-cs.fr>
2020-08-09 12:13:42 +0200

    Store collected tweets in json files

--- tweet_collection/__main__.py
+++ tweet_collection/__main__.py
@@ -4,6 +4,7 @@ from .get_queries import *
 from .collect_candidate_actuality_tweets import *
 from .collect_candidate_tweet_activity import *
 from .twitter_connection_setup import *
+from .tweets_storage import *
 
 # Example
 file_path = "./CandidateData/"
@@ -19,9 +20,11 @@ api = twitter_setup()
 queries = get_candidate_queries(Biden_ID, file_path, keyword_type)
 
 # Collect candidate actuality tweets
-get_tweets_from_candidates_search_queries(queries, api)
+tweets = get_tweets_from_candidates_search_queries(queries, api)
 #collect_candidate_actuality_tweets_by_streaming(queries, api)
 
 # Collect candidate's tweet activity
 #get_replies_to_candidate(Biden_ID, api)
 #print(collect_candidate_tweet_activity_by_streaming(twitter_api=api, candidate_id=Biden_ID))
+
+store_tweets(tweets, "./TweetsSerialized/tweet_activity_{}.json".format(Biden_ID))
