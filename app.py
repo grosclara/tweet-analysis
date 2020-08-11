2b882e54ab853a73e384a687b282b475a21664e4
Clara <clara.gros@student-cs.fr>
2020-08-11 15:40:06 +0200

    Dashboard general structure

--- app.py
+++ app.py
@@ -21,13 +21,52 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-colors = {
-    "graphBackground": "#F5F5F5",
-    "background": "#ffffff",
-    "text": "#000000"
-}
+# Multi-dropdown options
+from controls import COUNTIES, WELL_STATUSES, WELL_TYPES, WELL_COLORS
 
-app.layout = html.Div([
+# Create controls
+county_options = [{'label': str(COUNTIES[county]), 'value': str(county)}
+                  for county in COUNTIES]
+
+well_status_options = [{'label': str(WELL_STATUSES[well_status]),
+                        'value': str(well_status)}
+                       for well_status in WELL_STATUSES]
+
+well_type_options = [{'label': str(WELL_TYPES[well_type]),
+                      'value': str(well_type)}
+                     for well_type in WELL_TYPES]
+
+
+# Create global chart template
+mapbox_access_token = 'pk.eyJ1IjoiamFja2x1byIsImEiOiJjajNlcnh3MzEwMHZtMzNueGw3NWw5ZXF5In0.fk8k06T96Ml9CLGgKmk81w'
+
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(
+        l=30,
+        r=30,
+        b=20,
+        t=40
+    ),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation='h'),
+    title='Satellite Overview',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        style="light",
+        center=dict(
+            lon=-78.05,
+            lat=42.54
+        ),
+        zoom=7,
+    )
+)
+
+
+""" app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -49,7 +88,130 @@ app.layout = html.Div([
     ),
     dcc.Graph(id='Mygraph'),
     html.Div(id='output-data-upload')
-])
+]) """
+
+# Create app layout
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.Img(
+                    src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe.png",
+                    className='two columns',
+                ),
+                html.Div(
+                    [
+                        html.H2(
+                            'Twitter Predictor',
+
+                        ),
+                        html.H4(
+                            'Statistics on Twitter personalities',
+                        )
+                    ],
+
+                    className='eight columns'
+                ),
+                html.A(
+                    html.Button(
+                        "Learn More",
+                        id="learnMore"
+
+                    ),
+                    href="https://gitlab.com/grosclara/twitterpredictor",
+                    className="two columns"
+                )
+            ],
+            id="header",
+            className='row',
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Upload(
+                            id='upload-data',
+                            children=html.Div([
+                                'Drag and Drop or ',
+                                html.A('Select Files')
+                            ]),
+                            style={
+                                'width': '100%',
+                                'height': '60px',
+                                'lineHeight': '60px',
+                                'borderWidth': '1px',
+                                'borderStyle': 'dashed',
+                                'borderRadius': '5px',
+                                'textAlign': 'center',
+                                'margin': '10px'
+                            },
+                            # Allow multiple files to be uploaded
+                            multiple=True
+                        ),
+                    ]
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(id='candidate_info')
+                            ],
+                            className='pretty_container four columns',
+                        ),
+                        html.Div(
+                            [
+                                dcc.Graph(id='most_retweeted_tweet')
+                            ],
+                            className='pretty_container four columns',
+                        ),
+                        html.Div(
+                            [
+                                dcc.Graph(id='wordcloud')
+                            ],
+                            className='pretty_container four columns',
+                        ),
+                    ],
+                    className='row'
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                dcc.Graph(id='statistics_graph')
+                            ],
+                            className='pretty_container four columns',
+                        ),
+                        html.Div(
+                            [
+                                dcc.Graph(id='sentimental_analysis_graph')
+                            ],
+                            className='pretty_container eight columns',
+                        ),
+                    ],
+                    className="row"
+                ),
+            ],
+            className="row"
+        ),
+        html.Div(
+            [
+                html.H2(
+                    'Clara Gros, Student at CentraleSupélec',
+                ),
+                html.H4(
+                    'Source: Twitter API',
+                )
+            ],
+            className='eight columns'
+        ),
+    ],
+    id="mainContainer",
+    style={
+        "display": "flex",
+        "flex-direction": "column"
+    }
+)
+
 
 def parse_data(contents, filename):
     content_type, content_string = contents.split(',')
