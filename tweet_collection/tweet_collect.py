9859777b9ee4b816be81f0c1f22a5578ee82a0c0
Clara <clara.gros@student-cs.fr>
2020-08-13 09:59:37 +0200

    Remove keywords files

--- tweet_collection/tweet_collect.py
+++ tweet_collection/tweet_collect.py
@@ -3,24 +3,22 @@ import json
 from datetime import datetime
 import pandas as pd
 
-def get_candidate_queries(num_candidate, file_path, keyword_type):
+def get_candidate_queries(num_candidate, file_path):
     """
     Generate and return a list of string queries for the search Twitter API from the file file_path_num_candidate.txt
     :param num_candidate: the number of the candidate
     :param file_path: the path to the keyword and hashtag 
     files
-    :param type: type of the keyword, either "keywords" or "hashtags"
     :return: (list) a list of string queries that can be done to the search API independently
     """
 
     # Validation of parameters
-    assert keyword_type == 'hashtags' or keyword_type == 'keywords', "Invalid keyword_type parameter: either 'hashtags' or 'keywords'"
     assert type(num_candidate) == int, "num_candidate should be a int"
     assert num_candidate > 0, "num_candidate should be positive"
 
     try :
         # Open txt file
-        with open("{0}{1}_candidate_{2}.txt".format(file_path, keyword_type, num_candidate),'r',encoding = 'utf-8') as f:
+        with open(file_path,'r',encoding = 'utf-8') as f:
             # Retrieve words
             keywords = f.read().split(',')
             # Remove empty keywords
