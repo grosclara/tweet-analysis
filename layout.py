dfd4931ff2f1c907b2e21a5900ee24e178760c08
Clara <clara.gros@student-cs.fr>
2020-08-14 15:21:15 +0200

    Dashboard header design with bootstrap

---
+++ layout.py
@@ -0,0 +1,155 @@
+from io import BytesIO
+import base64
+import io
+import plotly.graph_objs as go
+import re
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from tweet_collection.tweet_collect import *
+from tweet_collection.twitter_connection_setup import *
+from tweet_analysis.tweet_analyze import *
+import dash_bootstrap_components as dbc
+from style import *
+
+input_groups = dbc.FormGroup(
+    [
+        dbc.InputGroup(
+            [
+                dbc.InputGroupAddon("@", addon_type="prepend"),
+                dbc.Input(id="input-screen-name", type="text", placeholder="Screen name")
+            ],
+        ),
+        html.Br(),
+        html.A(
+            dbc.Button(
+                children='Get Twitter ID',
+                color='primary',
+                block=True
+            ),
+            href='http://gettwitterid.com/',
+        ),
+        html.Br(),
+        dbc.InputGroup(
+            [
+                dbc.Input(id="input-id", type="number", placeholder="Twitter ID")
+            ],
+        ),
+        html.Br(),
+        dbc.Button(
+            id='submit_button',
+            n_clicks=0,
+            children='Submit',
+            color='primary',
+            block=True
+        ),
+    ]
+)
+
+sidebar = html.Div(
+    [
+        html.H2('Parameters', style=TEXT_STYLE),
+        html.Hr(),
+        input_groups
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content_first_row = dbc.Row([
+    dbc.Col(
+        dbc.Card([
+            dbc.CardImg(src="/static/images/placeholder286x180.png", top=True, id='profile-picture'),
+            dbc.CardBody([
+                html.H5("Most retweeted tweet", className="card-title"),
+                html.P("This card has an image on top, and a button below",
+                        className="card-text",
+                        id='tweet-content', 
+                ),
+                html.Footer(
+                    html.Small("Created at:", id='tweet-date', className="text-muted")
+                ),
+                html.Footer(
+                    html.Small("Rewteets count", id='rt-count', className="text-muted")
+                ),
+                html.Footer(
+                    html.Small("Likes count", id='like-count', className="text-muted")
+                ),
+            ]
+            ),
+        ]),
+        md=4
+    ),
+    dbc.Col(
+            dcc.Graph(id='tweets_evolution'), 
+            md=8
+    ),
+])
+
+content_second_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id='graph_1'), md=4
+        ),
+        dbc.Col(
+            dcc.Graph(id='graph_2'), md=4
+        ),
+        dbc.Col(
+            dcc.Graph(id='graph_3'), md=4
+        )
+    ]
+)
+
+content_third_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id='graph_4'), md=12,
+        )
+    ]
+)
+
+content_fourth_row = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Graph(id='graph_5'), md=6
+        ),
+        dbc.Col(
+            dcc.Graph(id='graph_6'), md=6
+        )
+    ]
+)
+
+header = html.Div(
+    [
+        dbc.Row(
+            [
+            dbc.Col(
+                html.H2('Twitter Personality Analytics', style=TEXT_STYLE),
+                width=9,
+                #center
+            ),
+            dbc.Col(
+                html.A(
+                    dbc.Button(
+                        children='Learn More',
+                        color='primary',
+                    ),
+                    href="https://gitlab.com/grosclara/twitterpredictor",  
+                ),
+                width=3
+            )
+            ]
+        ),
+        html.Hr()
+    ]
+)
+
+main_content = html.Div([
+    content_first_row,
+    content_second_row,
+    content_third_row,
+    content_fourth_row
+])
+
+content = html.Div([header, main_content], style = CONTENT_STYLE)
